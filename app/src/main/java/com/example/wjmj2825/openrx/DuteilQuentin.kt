83627b4897e09791bfda2be10497b712327fbfdf
quentin <quentin.duteil@orange.com>
2018-05-28 16:55:34 +0200

    continue trying to set multiple span in the response string

--- app/src/main/java/com/example/wjmj2825/openrx/DuteilQuentin.kt
+++ app/src/main/java/com/example/wjmj2825/openrx/DuteilQuentin.kt
@@ -3,33 +3,18 @@ package com.example.wjmj2825.openrx
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.util.Log
+import android.view.View
 import android.widget.Scroller
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
+import android.widget.Toast
 import io.reactivex.disposables.Disposable
 import io.reactivex.observers.DisposableObserver
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_duteil_quentin.*
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Path
-import java.util.concurrent.TimeUnit
-import android.text.method.LinkMovementMethod
-import android.R
-import android.widget.TextView
-import android.text.Spanned
-import android.text.TextPaint
-import android.content.Intent
-import android.graphics.Color
-import android.text.style.ClickableSpan
-import android.text.SpannableString
-import android.view.View
-import android.widget.Toast
 
 
 class DuteilQuentin : AppCompatActivity() {
@@ -104,25 +89,33 @@ class DuteilQuentin : AppCompatActivity() {
 
     fun updateUIWithListOfUsers(users: List<GithubUser>) {
 
-        val ss = SpannableString("Android is a Software stack")
+        val stringBuilder = SpannableStringBuilder()
+
         val clickableSpan = object : ClickableSpan() {
             override fun onClick(textView: View) {
                 Toast.makeText(this@DuteilQuentin,"prout",Toast.LENGTH_LONG).show()
             }
 
         }
-        ss.setSpan(clickableSpan, 22, 27, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        if (users.isEmpty())stringBuilder.append("Ce ne sont pas ces droides que vous cherchez")
+        else {
+            var i = 2
+            for (user in users) {
+                println("Avant user ${user.login}, i=$i, user.login.lengh=${user.login!!.length}")
+                stringBuilder.append("- ${user.login} \n")
+                stringBuilder.setSpan(clickableSpan, i, (i+user.login!!.length), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                i += user.login!!.length + 2
+                print("Après i=$i")
+            }
+        }
 
-        display_text.text = ss
+        val finalString = SpannableString(stringBuilder)
+
+        finalString.setSpan(clickableSpan, 1, 4, Spanned.SPAN_USER)
+        finalString.setSpan(clickableSpan, 5, 6, Spanned.SPAN_USER)
+        display_text.text = finalString
         display_text.movementMethod = LinkMovementMethod.getInstance()
 
-//        val stringBuilder = StringBuilder()
-//        if (users.isEmpty())stringBuilder.append("Ce ne sont pas ces droides que vous cherchez")
-//        else {
-//            for (user in users) {
-//                stringBuilder.append("- ${user.login} \n")
-//            }
-//        }
 //        updateUIWhenStopingHTTPRequests(stringBuilder.toString())
     }
 
@@ -138,197 +131,5 @@ class DuteilQuentin : AppCompatActivity() {
         super.onDestroy()
         disposeWhenDestroy()
     }
-
-}
-
-interface GithubService {
-
-    @GET("users/{username}/following")
-    fun getFollowing(@Path("username") username: String): Observable<List<GithubUser>>
-
-    @GET("/users/{username}")
-    fun getUserInfos(@Path("username") username: String): Observable<GithubUserInfo>
-
-    companion object {
-
-        val retrofit = Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build()
-    }
-}
-
-object GithubStreams {
-    fun streamFetchUserInfos(username: String): Observable<GithubUserInfo> {
-        val gitHubService = GithubService.retrofit.create(GithubService::class.java)
-        return gitHubService.getUserInfos(username)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .timeout(10, TimeUnit.SECONDS)
-    }
-
-    fun streamFetchUserFollowing(username: String): Observable<List<GithubUser>> {
-        val gitHubService = GithubService.retrofit.create(GithubService::class.java)
-        return gitHubService.getFollowing(username)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .timeout(10, TimeUnit.SECONDS)
-    }
-
-    fun streamFetchUserFollowingAndFetchFirstUserInfos(username: String): Observable<GithubUserInfo> {
-        return streamFetchUserFollowing(username).map { users -> users[0] }.flatMap { user -> streamFetchUserInfos(user.login.toString()) }
-    }
-
-}
-
-class GithubUser {
-
-    @SerializedName("login")
-    @Expose
-    var login: String? = null
-    @SerializedName("id")
-    @Expose
-    var id: Int? = null
-    @SerializedName("avatar_url")
-    @Expose
-    var avatarUrl: String? = null
-    @SerializedName("gravatar_id")
-    @Expose
-    var gravatarId: String? = null
-    @SerializedName("url")
-    @Expose
-    var url: String? = null
-    @SerializedName("html_url")
-    @Expose
-    var htmlUrl: String? = null
-    @SerializedName("followers_url")
-    @Expose
-    var followersUrl: String? = null
-    @SerializedName("following_url")
-    @Expose
-    var followingUrl: String? = null
-    @SerializedName("gists_url")
-    @Expose
-    var gistsUrl: String? = null
-    @SerializedName("starred_url")
-    @Expose
-    var starredUrl: String? = null
-    @SerializedName("subscriptions_url")
-    @Expose
-    var subscriptionsUrl: String? = null
-    @SerializedName("organizations_url")
-    @Expose
-    var organizationsUrl: String? = null
-    @SerializedName("repos_url")
-    @Expose
-    var reposUrl: String? = null
-    @SerializedName("events_url")
-    @Expose
-    var eventsUrl: String? = null
-    @SerializedName("received_events_url")
-    @Expose
-    var receivedEventsUrl: String? = null
-    @SerializedName("type")
-    @Expose
-    var type: String? = null
-    @SerializedName("site_admin")
-    @Expose
-    var siteAdmin: Boolean? = null
-
-}
-
-class GithubUserInfo {
-
-    @SerializedName("login")
-    @Expose
-    var login: String? = null
-    @SerializedName("id")
-    @Expose
-    var id: Int? = null
-    @SerializedName("avatar_url")
-    @Expose
-    var avatarUrl: String? = null
-    @SerializedName("gravatar_id")
-    @Expose
-    var gravatarId: String? = null
-    @SerializedName("url")
-    @Expose
-    var url: String? = null
-    @SerializedName("html_url")
-    @Expose
-    var htmlUrl: String? = null
-    @SerializedName("followers_url")
-    @Expose
-    var followersUrl: String? = null
-    @SerializedName("following_url")
-    @Expose
-    var followingUrl: String? = null
-    @SerializedName("gists_url")
-    @Expose
-    var gistsUrl: String? = null
-    @SerializedName("starred_url")
-    @Expose
-    var starredUrl: String? = null
-    @SerializedName("subscriptions_url")
-    @Expose
-    var subscriptionsUrl: String? = null
-    @SerializedName("organizations_url")
-    @Expose
-    var organizationsUrl: String? = null
-    @SerializedName("repos_url")
-    @Expose
-    var reposUrl: String? = null
-    @SerializedName("events_url")
-    @Expose
-    var eventsUrl: String? = null
-    @SerializedName("received_events_url")
-    @Expose
-    var receivedEventsUrl: String? = null
-    @SerializedName("type")
-    @Expose
-    var type: String? = null
-    @SerializedName("site_admin")
-    @Expose
-    var siteAdmin: Boolean? = null
-    @SerializedName("name")
-    @Expose
-    var name: String? = null
-    @SerializedName("company")
-    @Expose
-    var company: String? = null
-    @SerializedName("blog")
-    @Expose
-    var blog: String? = null
-    @SerializedName("location")
-    @Expose
-    var location: String? = null
-    @SerializedName("email")
-    @Expose
-    var email: String? = null
-    @SerializedName("hireable")
-    @Expose
-    var hireable: Boolean? = null
-    @SerializedName("bio")
-    @Expose
-    var bio: String? = null
-    @SerializedName("public_repos")
-    @Expose
-    var publicRepos: Int? = null
-    @SerializedName("public_gists")
-    @Expose
-    var publicGists: Int? = null
-    @SerializedName("followers")
-    @Expose
-    var followers: Int? = null
-    @SerializedName("following")
-    @Expose
-    var following: Int? = null
-    @SerializedName("created_at")
-    @Expose
-    var createdAt: String? = null
-    @SerializedName("updated_at")
-    @Expose
-    var updatedAt: String? = null
 
 }
